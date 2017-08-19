8da587d6249c22b4b41c6a9fb5871b66edfbc13d
diegosvi <dego1d@hotmail.com>
2017-08-19 12:25:58 -0500

    directores

--- src/app/components/directores/directores.component.ts
+++ src/app/components/directores/directores.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Director } from '../../interfaces/directores.interface';
+import { ActoresService } from '../../services/actores.service';
 
 @Component({
   selector: 'app-directores',
@@ -6,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class DirectoresComponent implements OnInit {
+  actor: Director[] = [];
+  constructor(private actorService: ActoresService) {
 
-  constructor() { }
-
+    this.actorService.consultarActor()
+      .subscribe(
+        resultado => {
+          this.actor = resultado;
+          console.log(resultado);
+        }
+      );
+  }
   ngOnInit() {
   }
 
---
+++ src/app/services/directores.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/RX';
+
+@Injectable()
+export class DirectoresService {
+
+  urlDirector: string = 'http://35.194.35.240/api';
+
+  constructor(private http: Http) { }
+
+  getDirector(indice: string) {
+    // let headers = new Headers({ 'Authorization': 'Bearer ' + this.authenticationService.token });
+    // let options = new RequestOptions({ headers: headers });
+    let urls = `${this.urlDirector}/${indice}`;
+
+    return this.http.get(urls)
+      .map(
+        res => {
+          console.log(res.json());
+          return res.json();
+        }
+      );
+  }
+
+  consultarDirector() {
+    // let headers = new Headers({ 'Authorization': 'Bearer ' + this.authenticationService.token });
+    // let options = new RequestOptions({ headers: headers });
+    return this.http.get(`${this.urlDirector}/directores`)
+      .map(
+        res => {
+          console.log( res.json() );
+          return res.json();
+        }
+      );
+  }
+}
