4431aea21806149cf2bb8eb1c8a49fd9f9b5c17e
mafer31 <m_a_fersua@hotmail.com>
2017-08-20 20:59:39 -0500

    peliculas

--- src/app/interfaces/peliculas.interface.ts
+++ src/app/interfaces/peliculas.interface.ts
@@ -2,11 +2,12 @@
  * Created by Diego Villavicencio on 20/8/2017.
  */
 export interface Pelicula {
-  num: string;
-  nombre: string;
-  contenido: string;
+  titulo: string;
+  sinopsis: string;
+  fechaLanzamiento: string;
   imagen: string;
-  calificacion: string;
+  financiero: any;
+
   // personaje: string;
   // nacimiento: {
   //   fecha: string;
