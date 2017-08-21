270f93cc8c1bbabb68298cb89b2b4ffe8f6b795f
AngeloDAvid <angelodavid95@hotmail.es>
2017-08-21 05:23:32 -0500

    Comentarios de peliculas

---
+++ AppMovilMove/src/interfaces/coment.interfaces.ts
@@ -0,0 +1,6 @@
+export  interface Coment{
+  usuario:string;
+  contenido:string;
+  pelicula:string;
+  id?:string;
+}
--- AppMovilMove/src/pages/home/home.ts
+++ AppMovilMove/src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController,ModalController } from 'ionic-angular';
+import {PeliculasProvider} from '../../providers/peliculas/peliculas';
+import {Pelicula} from  '../../interfaces/peliculas.interface';
+import {PeliculaaPage} from '../peliculaa/peliculaa';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  ListPeli: Pelicula[]=[];
+  constructor(public navCtrl: NavController, private proCtrl:PeliculasProvider,public modalCtrl: ModalController,) {
+    this.proCtrl.getMasvotada().subscribe(
+      resp=>{
+        for(let i=0; i<3;i++){
+          this.ListPeli.push(resp[i]);
+        }
+      }
+    );
+  }
+  vermas(pel:Pelicula){
+    let modal = this.modalCtrl.create(PeliculaaPage,{peli:pel});
+    modal.present();
   }
 
 }
